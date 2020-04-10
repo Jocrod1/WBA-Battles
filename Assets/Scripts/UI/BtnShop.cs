dfdfaaf3d13b9c2a4337af5a6db65ae56e6ee627
Raimon <ramonkoudsi@gmail.com>
2020-04-09 23:25:22 -0800

    menu dinamico

--- Assets/Scripts/UI/BtnShop.cs
+++ Assets/Scripts/UI/BtnShop.cs
@@ -10,7 +10,7 @@ public class BtnShop : MonoBehaviour
 
     private Vector3 shopPosition, optionsPosition, menuPosition;
 
-    private bool openShop, openOptions, openMenu, moving;
+    private bool openShop, closeShop, openOptions, closeOptions, moving=false;
 
     private void Start() {
 
@@ -23,7 +23,7 @@ public class BtnShop : MonoBehaviour
     public void OpenShop()
     {
 
-        if(Shop!=null && Options!=null && Menu!=null)
+        if(Shop!=null && Options!=null && Menu!=null && moving==false)
         {
             if(menuPosition==Menu.transform.position)
             {
@@ -32,14 +32,99 @@ public class BtnShop : MonoBehaviour
         }
     }
 
+    public void CloseShop()
+    {
+        if(Shop!=null && Options!=null && Menu!=null && moving==false)
+        {
+            if(menuPosition==Shop.transform.position)
+            {
+                closeShop=true;
+            }
+        }
+    }
+
+    public void OpenOptions()
+    {
+        if(Shop!=null && Options!=null && Menu!=null && moving==false)
+        {
+            if(menuPosition==Menu.transform.position)
+            {
+                openOptions=true;
+            }
+        }
+    }
+
+    public void CloseOptions()
+    {
+        if(Shop!=null && Options!=null && Menu!=null && moving==false)
+        {
+            if(menuPosition==Options.transform.position)
+            {
+                closeOptions=true;
+            }
+        }
+    }
+
     private void FixedUpdate() {
+
         if(openShop)
         {
+            moving=true;
             Shop.transform.position=Vector3.MoveTowards(Shop.transform.position, new Vector3(menuPosition.x, Shop.transform.position.y, 0), speed * Time.deltaTime);
             Options.transform.position=Vector3.MoveTowards(Options.transform.position, new Vector3(optionsPosition.x, Options.transform.position.y, 0), speed * Time.deltaTime);
             Menu.transform.position=Vector3.MoveTowards(Menu.transform.position, new Vector3(optionsPosition.x, Menu.transform.position.y, 0), speed * Time.deltaTime);
 
+            if(Shop.transform.position.x==menuPosition.x)
+            {
+                moving=false;
+                openShop=false;
 
+            }
+        }
+        if(closeShop)
+        {
+            moving=true;
+            Shop.transform.position=Vector3.MoveTowards(Shop.transform.position, new Vector3(shopPosition.x, Shop.transform.position.y, 0), speed * Time.deltaTime);
+            Options.transform.position=Vector3.MoveTowards(Options.transform.position, new Vector3(optionsPosition.x, Options.transform.position.y, 0), speed * Time.deltaTime);
+            Menu.transform.position=Vector3.MoveTowards(Menu.transform.position, new Vector3(menuPosition.x, Menu.transform.position.y, 0), speed * Time.deltaTime);
+
+            if(Shop.transform.position.x==shopPosition.x)
+            {
+                moving=false;
+                closeShop=false;
+            }
+        }
+
+
+
+        
+        if(openOptions)
+        {
+            moving=true;
+            Shop.transform.position=Vector3.MoveTowards(Shop.transform.position, new Vector3(shopPosition.x, Shop.transform.position.y, 0), speed * Time.deltaTime);
+            Options.transform.position=Vector3.MoveTowards(Options.transform.position, new Vector3(menuPosition.x, Options.transform.position.y, 0), speed * Time.deltaTime);
+            Menu.transform.position=Vector3.MoveTowards(Menu.transform.position, new Vector3(shopPosition.x, Menu.transform.position.y, 0), speed * Time.deltaTime);
+
+            if(Options.transform.position.x==menuPosition.x)
+            {
+                moving=false;
+                openOptions=false;
+
+            }
+        }
+
+        if(closeOptions)
+        {
+            moving=true;
+            Options.transform.position=Vector3.MoveTowards(Options.transform.position, new Vector3(optionsPosition.x, Options.transform.position.y, 0), speed * Time.deltaTime);
+            Shop.transform.position=Vector3.MoveTowards(Shop.transform.position, new Vector3(shopPosition.x, Shop.transform.position.y, 0), speed * Time.deltaTime);
+            Menu.transform.position=Vector3.MoveTowards(Menu.transform.position, new Vector3(menuPosition.x, Menu.transform.position.y, 0), speed * Time.deltaTime);
+
+            if(Menu.transform.position.x==menuPosition.x)
+            {
+                moving=false;
+                closeOptions=false;
+            }
         }
     }
 }
