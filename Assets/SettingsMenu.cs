2ab6710fdf968d16b872131d8061046a8869ac67
Raimon <ramonkoudsi@gmail.com>
2020-04-12 00:39:46 -0800

    playerprefabs y coins

---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    private int coins;
+    private AudioSource audioSrc;
+
+    private void Start() {
+
+        audioSrc=GetComponent<AudioSource>();
+
+        PlayerPrefs.SetInt("money", 1000);
+
+        coins=PlayerPrefs.GetInt("money");
+
+
+
+    }
+
+
+    private void Update() {
+        audioSrc.volume=PlayerPrefs.GetFloat("volume");
+
+        print(PlayerPrefs.GetFloat("volume"));
+    }
+
+    public void SetVolume(float vol)
+    {
+        PlayerPrefs.SetFloat("volume",vol);
+    }
+
+}
