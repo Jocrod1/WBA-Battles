589d99342f2e0a6abbdace5c8fbcc64a11ab7e48
Raimon <ramonkoudsi@gmail.com>
2020-04-12 18:03:35 -0800

    corregido fallos

--- Assets/CoinShopScript.cs
+++ Assets/CoinShopScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CoinShopScript : MonoBehaviour
 {
-    public GameObject Smoke, Shop;
+    public GameObject Smoke, Shop, Error;
     private int coins;
 
     private void Start() {
@@ -14,23 +14,59 @@ public class CoinShopScript : MonoBehaviour
 
     public void Coin100()
     {
-        coins-=100;
+        if(coins<100)
+        {
+            NoCoin();
+        }
+        else
+        {
+            coins-=100;
 
-        PlayerPrefs.SetInt("money", coins);
+            PlayerPrefs.SetInt("money", coins);
+        }
     }
 
     public void Coin200()
     {
-        coins-=200;
+        if(coins<200)
+        {
+            NoCoin();
+        }
+        else
+        {
+            coins-=200;
 
-        PlayerPrefs.SetInt("money", coins);
+            PlayerPrefs.SetInt("money", coins);
+        }
     }
 
-        public void Coin500()
+    public void Coin500()
     {
-        coins-=500;
+        if(coins<500)
+        {
+            NoCoin();
+        }
+        else
+        {
+            coins-=500;
 
-        PlayerPrefs.SetInt("money", coins);
+            PlayerPrefs.SetInt("money", coins);
+        }
+    }
+
+    private void NoCoin()
+    {
+        if(Smoke!=null && Error!=null)
+        {
+            Animator animator = Smoke.GetComponent<Animator>();
+            Animator animator2 = Error.GetComponent<Animator>();
+
+            if(animator!=null && animator2!=null)
+            {
+                animator.SetBool("Open", true);
+                animator2.SetBool("Open", true);
+            }
+        }
     }
 
 
@@ -41,7 +77,7 @@ public class CoinShopScript : MonoBehaviour
             Animator animator = Smoke.GetComponent<Animator>();
             Animator animator2 = Shop.GetComponent<Animator>();
 
-            if(animator!=null)
+            if(animator!=null && animator2!=null)
             {
                 animator.SetBool("Open", true);
                 animator2.SetBool("Open", true);
@@ -56,7 +92,22 @@ public class CoinShopScript : MonoBehaviour
             Animator animator = Smoke.GetComponent<Animator>();
             Animator animator2 = Shop.GetComponent<Animator>();
 
-            if(animator!=null)
+            if(animator!=null && animator2!=null)
+            {
+                animator.SetBool("Open", false);
+                animator2.SetBool("Open", false);
+            }
+        }
+    }
+
+    public void CloseError()
+    {
+        if(Smoke!=null && Error!=null)
+        {
+            Animator animator = Smoke.GetComponent<Animator>();
+            Animator animator2 = Error.GetComponent<Animator>();
+
+            if(animator!=null && animator2!=null)
             {
                 animator.SetBool("Open", false);
                 animator2.SetBool("Open", false);
