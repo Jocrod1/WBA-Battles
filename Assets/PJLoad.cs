8d1de008d683cce01e60e7f0034b6ffa1d223420
Raimon <ramonkoudsi@gmail.com>
2020-04-17 16:34:07 -0800

    Auto stash before merge of "Table" and "origin/Table"

--- Assets/PJLoad.cs
+++ Assets/PJLoad.cs
@@ -6,49 +6,59 @@ using TMPro;
  
 public class PJLoad : MonoBehaviour {
 
-    public GameObject Player, ImagePlayer;
+    public GameObject player, imagePlayer, cartelPlayer, cartelEnemy;
 
-    public GameObject Fight, Table;
+    public GameObject fight, table;
  
-    public Sprite champ1, champ2, champ3, champ4; 
+    public Sprite champ1, champ2, champ3, champ4;
+
+    public Sprite enemy1,enemy2,enemy3,enemy4,enemy5,enemy6,enemy7, player1,player2,player3,player4; 
  
-    public TextMeshProUGUI playerName, fightName;
+    public TextMeshProUGUI playerName, fightName, cartelName;
 
     private void Start() {
 
-        Player.transform.SetSiblingIndex(PlayerPrefs.GetInt("IDEnemy"));
+        player.transform.SetSiblingIndex(PlayerPrefs.GetInt("IDEnemy"));
 
         //Debug.Log(transform.GetSiblingIndex());
 
 
         if(PlayerPrefs.GetInt("IDPlayer")==1)
         {
-            ImagePlayer.GetComponent<Image>().sprite = champ1;
+            imagePlayer.GetComponent<Image>().sprite = champ1;
+            cartelPlayer.GetComponent<Image>().sprite = player1;
             playerName.GetComponent<TextMeshProUGUI>().text="Arlen Smith";
+            cartelName.GetComponent<TextMeshProUGUI>().text="Arlen Smith";
         }
         else if(PlayerPrefs.GetInt("IDPlayer")==2)
         {
-            ImagePlayer.GetComponent<Image>().sprite = champ2;
+            imagePlayer.GetComponent<Image>().sprite = champ2;
+            cartelPlayer.GetComponent<Image>().sprite = player2;
             playerName.GetComponent<TextMeshProUGUI>().text="Daga Johar";
+            cartelName.GetComponent<TextMeshProUGUI>().text="Daga Johar";
         }
         else if(PlayerPrefs.GetInt("IDPlayer")==3)
         {
-            ImagePlayer.GetComponent<Image>().sprite = champ3;
+            imagePlayer.GetComponent<Image>().sprite = champ3;
+            cartelPlayer.GetComponent<Image>().sprite = player3;
             playerName.GetComponent<TextMeshProUGUI>().text="Irina Jones";
+            cartelName.GetComponent<TextMeshProUGUI>().text="Irina Jones";
         }
         else if(PlayerPrefs.GetInt("IDPlayer")==4)
         {
-            ImagePlayer.GetComponent<Image>().sprite = champ4;
+            imagePlayer.GetComponent<Image>().sprite = champ4;
+            cartelPlayer.GetComponent<Image>().sprite = player4;
             playerName.GetComponent<TextMeshProUGUI>().text="Angenis Nadai";
+            cartelName.GetComponent<TextMeshProUGUI>().text="Angenis Nadai";
         }
     }
 
     public void OpenFight()
     {
-        if(Fight!=null && Table!=null)
+        if(fight!=null && table!=null)
         {
-            Animator animator2 = Fight.GetComponent<Animator>();
-            Animator animator3 = Table.GetComponent<Animator>();
+            Animator animator2 = fight.GetComponent<Animator>();
+            Animator animator3 = table.GetComponent<Animator>();
 
             if(animator2!=null && animator3!=null)
             {
@@ -58,6 +68,7 @@ public class PJLoad : MonoBehaviour {
                 if(PlayerPrefs.GetInt("IDEnemy")==7)
                 {
                     fightName.GetComponent<TextMeshProUGUI>().text="El Calvo";
+                    cartelEnemy.GetComponent<Image>().sprite = enemy1;
                 }
             }
         }
