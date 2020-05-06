0d0c411361b1022b11852c1c0aa75b9f0389f8ec
Jose Pereira <wallbangstudios4.9@gmail.com>
2020-05-06 05:41:46 -0400

    Winners Panels

--- Assets/Scripts/UI/ResultsManager.cs
+++ Assets/Scripts/UI/ResultsManager.cs
@@ -32,63 +32,77 @@ public class ResultsManager : MonoBehaviour
 
     [Header("Panel List")]
     public List<MotivationPanel> playersLoosePanels;
+    public List<MotivationPanel> playersWinPanels;
+
+    public enum Result {
+        Win,
+        Defeat
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        if (Results.GameOver) {
-            Results.GameOver = false;
-            idEnemy= PlayerPrefs.GetInt("IDEnemy");
+        Results.GameOver = false;
+        idEnemy = PlayerPrefs.GetInt("IDEnemy");
 
-            //agregar una hora al tiempo en que termino la pelea
-            oneHour= System.DateTime.Now;
-            duration = new System.TimeSpan(0, 0, 0, 60);
-            timeNextFight = oneHour.Add(duration);
+        //agregar una hora al tiempo en que termino la pelea
+        oneHour = System.DateTime.Now;
+        duration = new System.TimeSpan(0, 0, 0, 60);
+        timeNextFight = oneHour.Add(duration);
 
-            if (Results.Win) {
-                ResultText.text = "YOU WON!!!";
-                ResultText.color = new Color(203f, 166f, 54f);
+        if (Results.Win)
+        {
+            ResultText.text = "YOU WON!!!";
+            ResultText.color = new Color(203f, 166f, 54f);
 
-                PlayerPrefs.SetString("WaitOneHour", timeNextFight.ToString());
+            PlayerPrefs.SetString("WaitOneHour", timeNextFight.ToString());
 
-                if(idEnemy>=10 && idEnemy<=12)
-                {
-                    idEnemy=idEnemy-1;
-                    PlayerPrefs.SetInt("IDEnemy", idEnemy);
-                }
-                else if(idEnemy==0)
-                {
-                    //PlayerPrefs.SetInt("IDEnemy", 10);
-                }
-                else if(idEnemy>=1 && idEnemy<=7)
-                {
-                    idEnemy=idEnemy-1;
-                    PlayerPrefs.SetInt("IDEnemy", idEnemy);
-                }
-            }
-            else {
-                ResultText.text = "YOU LOOSE";
-                ResultText.color = new Color(140f, 0, 0);
+            SetPanel(true);
 
-                PlayerPrefs.SetString("WaitOneHour", timeNextFight.ToString());
-
-                SetLoosePanel();
-
-                idEnemy = 7;
+            if (idEnemy >= 10 && idEnemy <= 12)
+            {
+                idEnemy = idEnemy - 1;
                 PlayerPrefs.SetInt("IDEnemy", idEnemy);
             }
-
-            PlayerPrefs.SetInt("health", 0);
-            PlayerPrefs.SetInt("stamina", 0);
-            PlayerPrefs.SetInt("damage", 0);
-
+            else if (idEnemy == 0)
+            {
+                //PlayerPrefs.SetInt("IDEnemy", 10);
+            }
+            else if (idEnemy >= 1 && idEnemy <= 7)
+            {
+                idEnemy = idEnemy - 1;
+                PlayerPrefs.SetInt("IDEnemy", idEnemy);
+            }
         }
+        else
+        {
+            ResultText.text = "YOU LOOSE";
+            ResultText.color = new Color(140f, 0, 0);
+
+            PlayerPrefs.SetString("WaitOneHour", timeNextFight.ToString());
+
+            SetPanel(false);
+
+            idEnemy = 7;
+            PlayerPrefs.SetInt("IDEnemy", idEnemy);
+        }
+
+        PlayerPrefs.SetInt("health", 0);
+        PlayerPrefs.SetInt("stamina", 0);
+        PlayerPrefs.SetInt("damage", 0);
+
 
     }
 
-    public void SetLoosePanel() {
+    public int Idp;
+    public int idm;
+
+    public void SetPanel(bool win) {
         int IdPlayer = PlayerPrefs.GetInt("IDPlayer", 0);
         int IdMotivation = PlayerPrefs.GetInt("IDMotivation", 0);
+
+        Idp = IdPlayer - 1;
+        idm = IdMotivation - 1;
 
         if (IdMotivation == 0 || IdPlayer == 0) {
             print("ERROR: Id nulo");
@@ -98,7 +112,10 @@ public class ResultsManager : MonoBehaviour
         IdPlayer--;
         IdMotivation--;
 
-        ImagePanel.sprite = playersLoosePanels[IdPlayer].Motivation[IdMotivation];
+        if (!win)
+            ImagePanel.sprite = playersLoosePanels[Idp].Motivation[idm];
+        else
+            ImagePanel.sprite = playersWinPanels[Idp].Motivation[idm];
     }
 
     public void goTable()
